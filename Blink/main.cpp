f26a0ead8362e64c6c65aa846acf39b85605a003
CorvusPrudens <corvusprudens@gmail.com>
2023-04-24 12:11:19 -0600

    Removed code comments and aligned to guide

--- Blink/main.cpp
+++ Blink/main.cpp
@@ -1,22 +1,17 @@
-/** Basic Daisy Blink Example
- *
- *  This example blinks toggles the state of an LED every half a second.
- */
 #include "daisy_seed.h"
 
-using namespace daisy;
+daisy::DaisySeed hardware;
 
-DaisySeed hw;
+int main()
+{
+    hardware.Init();
 
-int main() {
-  hw.Init();
-  bool led_state = false;
-  while (1) {
-    /** Set the USR LED to the current state */
-    hw.SetLed(led_state);
-    /** Toggle the state from true/false, or vice versa */
-    led_state = !led_state;
-    /** Delay for the specified number of milliseconds */
-    System::Delay(500);
-  }
+    bool led_state = false;
+
+    while (true)
+    {
+        hardware.SetLed(led_state);
+        led_state = !led_state;
+        daisy::System::Delay(500);
+    }
 }
